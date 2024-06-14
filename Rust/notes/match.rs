6460aa38f069c5a4e09166d32d327bfd0398ddff
hall <ohjunk2001@outlook.com>
2024-06-14 10:28:22 +0800

    Create match.rs

---
+++ Rust/notes/match.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState), // 25美分硬币
+}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn main() {
+    let coin1=Coin::Quarter(UsState::Alabama);
+    println!("{}",value_in_cents(coin1));
+}

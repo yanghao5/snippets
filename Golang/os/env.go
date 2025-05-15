a3a9db2e26d01f830c3c6a6bc15df2e7d7ba64c9
yanghao5 <yanghao.buffer@outlook.com>
2025-05-15 12:13:10 +0800

    202505151213

--- Golang/os/env.go
+++ Golang/os/env.go
@@ -1,17 +1,17 @@
 // os.Getenv
 // If the env variable does not exist, return null value
-value := os.Getenv("MY_ENV_VAR")
-if value == "" {
+v := os.Getenv("MY_ENV_VAR")
+if v == "" {
 	fmt.Println("MY_ENV_VAR not exist")
 } else {
-	fmt.Println("MY_ENV_VAR =", value)
+	fmt.Println("MY_ENV_VAR =", v)
 }
 
 // os.LookupEnv
 // if the env variable does not exist, return err
-value, exists := os.LookupEnv("MY_ENV_VAR")
-if !exists {
+v, ok := os.LookupEnv("MY_ENV_VAR")
+if !ok {
 	fmt.Println("环境变量 MY_ENV_VAR 不存在")
 } else {
-	fmt.Println("MY_ENV_VAR =", value)
+	fmt.Println("MY_ENV_VAR =", v)
 }
